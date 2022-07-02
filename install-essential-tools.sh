aff794a27ce3a9073da2800a8b339dc34fce075e
vancanhuit <vancanhuit@gmail.com>
2022-07-02 21:17:35 +0700

    Update scripts

--- install-essential-tools.sh
+++ install-essential-tools.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update
-sudo apt-get --yes upgrade
-sudo apt-get --yes install git vim curl wget htop tmux
+sudo apt-get --quiet --yes dist-upgrade
+sudo apt-get --quiet --yes install git vim curl wget htop tmux
 
 cp -v /vagrant/.vimrc ~/.vimrc
 cp -v /vagrant/.tmux.conf ~/.tmux.conf
--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update
-sudo apt-get install --yes apt-transport-https ca-certificates curl
+sudo apt-get install --quiet --yes apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install --yes kubelet kubeadm kubectl
+sudo apt-get install --quiet --yes kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
