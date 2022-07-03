f5c575bcf070000790d57ad1bb5a269a49a408b9
vancanhuit <vancanhuit@gmail.com>
2022-07-03 17:49:05 +0700

    Minor update

--- install-essential-tools.sh
+++ install-essential-tools.sh
@@ -9,7 +9,7 @@ sudo apt-get --quiet --yes install git vim curl wget htop tmux jq
 wget https://github.com/mikefarah/yq/releases/download/v4.25.3/yq_linux_amd64
 sudo install -m 0755 yq_linux_amd64 /usr/local/bin/yq
 
-rm -v yq_linux_amd64
+rm -fv yq_linux_amd64
 
 cp -v /vagrant/.vimrc ~/.vimrc
 cp -v /vagrant/.tmux.conf ~/.tmux.conf
